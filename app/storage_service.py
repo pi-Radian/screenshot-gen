2724367f6a670f08d1fc550c94ba0e88a6044b00
pi-Radian <83527975+pi-Radian@users.noreply.github.com>
2022-09-02 08:52:14 +0530

    Added cloudinary

--- app/storage_service.py
+++ app/storage_service.py
@@ -1,4 +1,22 @@
+import cloudinary
 
+cloudinary.config(
+cloud_name = "xxxx",
+api_key = "xxxxxxxxxxxx",
+api_secret = "xxxxxxxxxxx",
+api_proxy = "http://proxy.server:9999"
+)
+
+import cloudinary.uploader
+import cloudinary.api
+
+
+# cloudinary.uploader.upload("dog.mp4", 
+#   folder = "myfolder/mysubfolder/", 
+#   public_id = "my_dog",
+#   overwrite = true, 
+#   notification_url = "https://mysite.example.com/notify_endpoint", 
+#   resource_type = "video")
 
 
 def save_img_to_bucket(random_path):
